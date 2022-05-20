7f1993cd8eb5b04ffb9896451a89840383396de7
PabloGTUOC <105376688+PabloGTUOC@users.noreply.github.com>
2022-05-20 17:18:04 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,25 +38,25 @@ var searchInput = document.getElementById("search");
 searchInput.addEventListener('keyup', function(){
   var search = this.value;
     const filtercurrencies = extractCurrenciesFromSearch(search);
-      // Clear the list
-      var app = new cleanUpLoad(resultdomin);
-      //Add new list
-      var newList = new loadList(app.listDom);
-      filtercurrencies.forEach(element => {
+    // Clear the list
+    var app = new cleanUpLoad(resultdomin);
+    //Add new list
+    var newList = new loadList(app.listDom);
+    filtercurrencies.forEach(element => {
         app.listDom.getElementsByClassName('currencylist')[0].append(increaselist(element));
     });
+
 });
 
+    /*if (app.listDom.getElementsByClassName('currencylist')[0].length > 0) {
+        let newT = updateTitle(removetitle, "You can click for further details");
+        resultdomin.getElementsByClassName('results__header')[0].appendChild(newT);
+    } else {
+        let newT = updateTitle(removetitle, "There is no matching results");
+        resultdomin.getElementsByClassName('results__header')[0].appendChild(newT);
+    }*/
+
 const currencieDetails = await initDetails(clickDetails);
-
-
-if (app.listDom.getElementsByClassName('currencylist')[0]) {
-    let newT = updateTitle(removetitle, "You can click for further details");
-    resultdomin.getElementsByClassName('results__header')[0].appendChild(newT);
-} else {
-    let newT = updateTitle(removetitle, "There is no matching results");
-    resultdomin.getElementsByClassName('results__header')[0].appendChild(newT);
-}
 
 
 function extractCurrenciesFromSearch(query){
